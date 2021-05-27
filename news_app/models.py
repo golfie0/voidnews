cb26c2606f40d651260ab8019004cb041afd4838
golfie0 <boomrus1@mail.ru>
2021-05-27 21:17:20 +0300

    deleted:    database.mwb 	renamed:    db.sqlite3 -> mydatabase 	modified:   news_app/__pycache__/models.cpython-39.pyc 	modified:   news_app/__pycache__/views.cpython-39.pyc 	new file:   news_app/migrations/0001_initial.py 	new file:   news_app/migrations/0002_auto_20210525_0443.py 	new file:   news_app/migrations/0003_post_dnews_post_itproger.py 	new file:   news_app/migrations/0004_auto_20210526_1431.py 	new file:   news_app/migrations/__pycache__/0001_initial.cpython-39.pyc 	new file:   news_app/migrations/__pycache__/0002_auto_20210525_0443.cpython-39.pyc 	new file:   news_app/migrations/__pycache__/0003_post_dnews_post_itproger.cpython-39.pyc 	new file:   news_app/migrations/__pycache__/0004_auto_20210526_1431.cpython-39.pyc 	modified:   news_app/migrations/__pycache__/__init__.cpython-39.pyc 	modified:   news_app/models.py 	modified:   news_app/templates/news_app/home.html 	deleted:    news_app/templates/news_app/masonry-layout/README.md 	deleted:    news_app/templates/news_app/masonry-layout/dist/masonry.pkgd.js 	deleted:    news_app/templates/news_app/masonry-layout/dist/masonry.pkgd.min.js 	deleted:    news_app/templates/news_app/masonry-layout/masonry.js 	deleted:    news_app/templates/news_app/masonry-layout/package.json 	modified:   news_app/views.py 	new file:   test.py 	modified:   voidnews/__pycache__/settings.cpython-39.pyc 	modified:   voidnews/settings.py 	modified:   voidnews/static/style.css

--- news_app/models.py
+++ news_app/models.py
@@ -1,8 +1,20 @@
-#from django.db import models
+from django.db import models
 
 
 # Create your models here.
-# class Article(models.Model):
-#     article_title = models.CharField('Заголовок статьи', max_length=200)
-#     article_image = models.CharField('Ссылка на картинку', max_length=300)
-#     pub_date = models.DateTimeField('Дата')
+class post_habr(models.Model):
+    post_title = models.CharField('Заголовок статьи', max_length=300, unique=True, null=True)
+    post_source = models.URLField('Ссылка на источник', max_length=500, unique=True, null=True)
+    pub_date = models.DateTimeField('Дата', null=True)
+
+
+class post_tproger(models.Model):
+    post_title = models.CharField('Заголовок статьи', max_length=300, unique=True, null=True)
+    post_source = models.URLField('Ссылка на источник', max_length=500, unique=True, null=True)
+    pub_date = models.DateTimeField('Дата', null=True)
+
+
+class post_dnews(models.Model):
+    post_title = models.CharField('Заголовок статьи', max_length=300, unique=True, null=True)
+    post_source = models.URLField('Ссылка на источник', max_length=500, unique=True, null=True)
+    pub_date = models.DateTimeField('Дата', null=True)
--- voidnews/settings.py
+++ voidnews/settings.py
@@ -76,14 +76,12 @@ WSGI_APPLICATION = 'voidnews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       # 'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'database',
-        #'USER': 'admin',
-        #'PASSWORD': '190102Vlad',
-        #'HOST': 'localhost',
-        #'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
